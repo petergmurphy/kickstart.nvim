9b31b68b0581358ca1a76fc7aa1b575bf1391f0b
petergmurphy <petergmurphy212@gmail.com>
2024-03-28 00:51:38 +0000

    Initial setup and neotree configuration

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,16 @@
+vim.keymap.set('n', '<C-Z>', ':Neotree toggle<cr>', { desc = 'Open Neotree on left side' })
+
+-- vim.keymap.set('n', '<leader>bt', ':Neotree float buffers<cr>', { desc = 'Show git status in Neotree' }) DIDNT SEEM TO WORK
+
+vim.keymap.set('n', '<leader>gt', ':Neotree float git_status<cr>', { desc = 'Show git status in Neotree' })
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+}
